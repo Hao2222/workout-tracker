ceb12cf05cf43306daab646de3b836082e1c100f
Hao <hao09@yahoo.com>
2021-01-28 20:04:55 -0800

    deploy

--- server.js
+++ server.js
@@ -2,13 +2,7 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 
-mongoose.connect(
-  process.env.MONGODB_URI ||"mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useCreateIndex: true,
-  useFindAndModify: false
-});
+
 
 const PORT = 3000;
 
@@ -21,7 +15,13 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-
+mongoose.connect(
+  process.env.MONGODB_URI ||"mongodb://localhost/workout-tracker", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
 
 // routes
 app.use(require("./routes/api.js"));
