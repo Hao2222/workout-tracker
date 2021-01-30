c59e9503aae2de160d92771991d05cb0964125f8
Hao <hao09@yahoo.com>
2021-01-29 17:39:41 -0800

    deploy

---
+++ config/config.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+    development: {
+      PORT: process.env.PORT,
+    } };
+  
--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
-const path = require('path');
-require('dotenv').config({path:'/.env'});
 
+require("dotenv").config();
 
-const PORT = 3000;
+// const PORT = 3000;
 
 const app = express();
 
@@ -16,9 +15,9 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-var MONGODB_URI = 'mongodb+srv://haolam:hana123@cluster0.1pczb.mongodb.net/workout?retryWrites=true&w=majority';
+
 mongoose.connect(
-  MONGODB_URI || 'mongodb://localhost/workout', {
+ process.env.MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -35,6 +34,8 @@ mongoose.connect(
 // routes
 app.use(require("./routes/api.js"));
 app.use(require("./routes/view.js"));
+
+const PORT = process.env.PORT 
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
