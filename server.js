16d33c143c319df56f1ee37b4d54d5ffb819228b
Hao <hao09@yahoo.com>
2021-01-29 16:35:31 -0800

    deploy

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+const path = require('path');
+require('dotenv').config({path:'/.env'});
+
 
 const PORT = 3000;
 
@@ -13,8 +16,10 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
+var MONGODB_URI = 'mongodb+srv://haolam:hana123@cluster0.1pczb.mongodb.net/workout?retryWrites=true&w=majority';
 mongoose.connect(
-  process.env.MONGODB_URI ||'mongodb://localhost/workout', {
+
+  MONGODB_URI || 'mongodb://localhost/workout', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
@@ -23,8 +28,8 @@ mongoose.connect(
 // mongoose.connect(
 //   'mongodb+srv://haolam:hana123@cluster0.1pczb.mongodb.net/workout?retryWrites=true&w=majority', {
 //   useNewUrlParser: true,
-//   // useUnifiedTopology: true,
-//   // useCreateIndex: true,
+//   useUnifiedTopology: true,
+//   useCreateIndex: true,
 //   useFindAndModify: false
 // });
 
